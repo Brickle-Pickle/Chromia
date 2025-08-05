05c350078b9ecd1d578474488377bf4dfea238fc
BricklePickle_Dev <bricklepicklegs@gmail.com>
2025-08-05 18:46:32 +0200

    Fix npm ci error - use bcryptjs and update build config

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,4 @@
 import User from "../models/User.js";
-import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import express from "express";
 import { authenticateToken } from "../middlewares/authMiddleware.js";
